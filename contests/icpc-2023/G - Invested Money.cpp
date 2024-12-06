e76b4273a89db500a099b6cc04a3764e6d4a37cd
MorveN11 <morales.patty.jose@gmail.com>
2024-12-06 10:27:24 -0400

    feat: add last exercises

---
+++ contests/icpc-2023/G - Invested Money.cpp
@@ -0,0 +1,126 @@
+#ifdef LOCALDEB
+#include "/home/morven/code/competitive-programming/conf/debug.h"
+#define line cerr << "-------------------" << endl;
+#else
+#define deb(x...)
+#define line
+#endif
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define F first
+#define S second
+#define pb push_back
+#define sz size
+#define all(x) begin(x), end(x)
+#define sortt(x) sort(all(x))
+#define each(x, xs) for (auto &x : (xs))
+#define rep(i, be, en) for (__typeof(en) i = (be); i < (en); i++)
+#define per(i, be) for (__typeof(be) i = (be) - 1; i >= 0; i--)
+#define readline(x) getline(cin, x)
+#define strInt(str) stoi(str)
+#define chrInt(chr) (int)chr - 48
+#define ensp(i, n) (" \n"[i == n - 1])
+#define spne(i) (" \n"[i == 0])
+
+using ll = long long;
+using ld = long double;
+using lli = long long int;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+using ti = tuple<int, int, int>;
+using tl = tuple<ll, ll, ll>;
+using vi = vector<int>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+using vs = vector<string>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vpi = vector<pair<int, int>>;
+using vpl = vector<pair<ll, ll>>;
+template <class T>
+using pq = priority_queue<T>;
+template <class T>
+using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+const ll INF = LONG_LONG_MAX;
+const int inf = INT_MAX;
+const ld PI = acos(-1);
+const vector<int> DX{1, 0, -1, 0}, DY{0, 1, 0, -1};
+ll testId = 0;
+
+void _() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+#ifdef LOCAL
+    freopen("/home/morven/code/competitive-programming/conf/main.in", "r",
+            stdin);
+    freopen("/home/morven/code/competitive-programming/conf/main.out", "w",
+            stdout);
+    freopen("/home/morven/code/competitive-programming/conf/main.err", "w",
+            stderr);
+#endif
+}
+
+void init();
+
+void solve();
+
+int main() {
+    _();
+    init();
+    ll T = 1;
+
+    // cinn(T);
+    rep(t, 0, T) {
+        testId++;
+        solve();
+    }
+
+    return 0;
+}
+
+void init() {}
+
+map<string, int> days = {
+    {"Mon", 1}, {"Tue", 2}, {"Wed", 3}, {"Thu", 4},
+    {"Fri", 5}, {"Sat", 6}, {"Sun", 7},
+};
+
+void solve() {
+    string sd;
+    int d, q;
+
+    cin >> sd >> q;
+
+    d = days[sd];
+
+    pqg<int> pq;
+
+    rep(i, 0, q) {
+        int x, nd, wd;
+
+        cin >> x;
+
+        if (x % 30 == 0 && x >= 30) {
+            x = 30;
+        } else {
+            x %= 30;
+        }
+
+        nd = 30 - x;
+
+        wd = (d + nd) % 7;
+
+        if (wd == 6 || wd == 7) {
+            nd += 8 - wd;
+        }
+
+        pq.push(nd);
+    }
+
+    cout << pq.top() << endl;
+}
---
+++ contests/icpc-2023/N - Lexicographical Challenge.cpp
@@ -0,0 +1,116 @@
+#ifdef LOCALDEB
+#include "/home/morven/code/competitive-programming/conf/debug.h"
+#define line cerr << "-------------------" << endl;
+#else
+#define deb(x...)
+#define line
+#endif
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define F first
+#define S second
+#define pb push_back
+#define sz size
+#define all(x) begin(x), end(x)
+#define sortt(x) sort(all(x))
+#define each(x, xs) for (auto &x : (xs))
+#define rep(i, be, en) for (__typeof(en) i = (be); i < (en); i++)
+#define per(i, be) for (__typeof(be) i = (be) - 1; i >= 0; i--)
+#define readline(x) getline(cin, x)
+#define strInt(str) stoi(str)
+#define chrInt(chr) (int)chr - 48
+#define ensp(i, n) (" \n"[i == n - 1])
+#define spne(i) (" \n"[i == 0])
+
+using ll = long long;
+using ld = long double;
+using lli = long long int;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+using ti = tuple<int, int, int>;
+using tl = tuple<ll, ll, ll>;
+using vi = vector<int>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+using vs = vector<string>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vpi = vector<pair<int, int>>;
+using vpl = vector<pair<ll, ll>>;
+template <class T>
+using pq = priority_queue<T>;
+template <class T>
+using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+const ll INF = LONG_LONG_MAX;
+const int inf = INT_MAX;
+const ld PI = acos(-1);
+const vector<int> DX{1, 0, -1, 0}, DY{0, 1, 0, -1};
+ll testId = 0;
+
+void _() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+#ifdef LOCAL
+    freopen("/home/morven/code/competitive-programming/conf/main.in", "r",
+            stdin);
+    freopen("/home/morven/code/competitive-programming/conf/main.out", "w",
+            stdout);
+    freopen("/home/morven/code/competitive-programming/conf/main.err", "w",
+            stderr);
+#endif
+}
+
+void init();
+
+void solve();
+
+int main() {
+    _();
+    init();
+    ll T = 1;
+
+    // cinn(T);
+    rep(t, 0, T) {
+        testId++;
+        solve();
+    }
+
+    return 0;
+}
+
+void init() {}
+
+void solve() {
+    string s;
+    int n;
+
+    cin >> s >> n;
+
+    vector<pqg<char>> v(n, pqg<char>());
+
+    int it = 0;
+
+    rep(i, 0, s.sz()) {
+        v[it++].push(s[i]);
+
+        if (it == n) it = 0;
+    }
+
+    it = 0;
+
+    rep(i, 0, s.sz()) {
+        char a = v[it].top();
+        v[it++].pop();
+
+        cout << a;
+
+        if (it == n) it = 0;
+    }
+
+    cout << endl;
+}
